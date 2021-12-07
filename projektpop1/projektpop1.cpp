e41c44f31595c0358c7fe17531cabfa1157d2c28
KYUNEX <kubalo@o2.pl>
2021-12-07 14:52:48 +0100

    dodanie podzielenia na 4 czesci

--- projektpop1/projektpop1.cpp
+++ projektpop1/projektpop1.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 // Funkcja rysuje planszę gry w kółko i krzyżyk
 //---------------------------------------------
+
+// ─┐└┴┬├┼│┤┘┌
+// ╚╣║╗╝╔╩╦╠═╬
+
+
 void plansza(char t[])
 {
     for (int i = 1; i <= 36; i++)
@@ -12,11 +17,94 @@ void plansza(char t[])
         cout << " " << t[i] << " ";
         if (i % 6)
             cout << "|";
+        //cout << "║";
         else if (i != 36)
             cout << "\n---+---+---+---+---+---\n";
+        //cout <<"╠═╬╠═╬╠═╬╠═╬";
         else cout << endl;
     }
 }
+
+
+// ─┐└┴┬├┼│┤┘┌
+// ╚╣║╗╝╔╩╦╠═╬
+
+int cwiartkanaidx(char cwiartka, int nr) {
+    if (cwiartka == 'q' && nr == 1)
+        return 13;
+    if (cwiartka == 'q' && nr == 2)
+        return 14;
+    if (cwiartka == 'q' && nr == 3)
+        return 15;
+    if (cwiartka == 'q' && nr == 4)
+        return 7;
+    if (cwiartka == 'q' && nr == 5)
+        return 8;
+    if (cwiartka == 'q' && nr == 6)
+        return 9;
+    if (cwiartka == 'q' && nr == 7)
+        return 1;
+    if (cwiartka == 'q' && nr == 8)
+        return 2;
+    if (cwiartka == 'q' && nr == 9)
+        return 3;
+    if (cwiartka == 'w' && nr == 1)
+        return 16;
+    if (cwiartka == 'w' && nr == 2)
+        return 17;
+    if (cwiartka == 'w' && nr == 3)
+        return 18;
+    if (cwiartka == 'w' && nr == 4)
+        return 10;
+    if (cwiartka == 'w' && nr == 5)
+        return 11;
+    if (cwiartka == 'w' && nr == 6)
+        return 12;
+    if (cwiartka == 'w' && nr == 7)
+        return 4;
+    if (cwiartka == 'w' && nr == 8)
+        return 5;
+    if (cwiartka == 'w' && nr == 9)
+        return 6;
+    if (cwiartka == 'a' && nr == 1)
+        return 31;
+    if (cwiartka == 'a' && nr == 2)
+        return 32;
+    if (cwiartka == 'a' && nr == 3)
+        return 33;
+    if (cwiartka == 'a' && nr == 4)
+        return 25;
+    if (cwiartka == 'a' && nr == 5)
+        return 26;
+    if (cwiartka == 'a' && nr == 6)
+        return 27;
+    if (cwiartka == 'a' && nr == 7)
+        return 19;
+    if (cwiartka == 'a' && nr == 8)
+        return 20;
+    if (cwiartka == 'a' && nr == 9)
+        return 21;
+    if (cwiartka == 's' && nr == 1)
+        return 34;
+    if (cwiartka == 's' && nr == 2)
+        return 35;
+    if (cwiartka == 's' && nr == 3)
+        return 36;
+    if (cwiartka == 's' && nr == 4)
+        return 28;
+    if (cwiartka == 's' && nr == 5)
+        return 29;
+    if (cwiartka == 's' && nr == 6)
+        return 30;
+    if (cwiartka == 's' && nr == 7)
+        return 22;
+    if (cwiartka == 's' && nr == 8)
+        return 23;
+    if (cwiartka == 's' && nr == 9)
+        return 24;
+}
+
+
 
 // Funkcja zwraca true, jeśli nastąpiła
 // wygrana danego zawodnika
@@ -81,8 +169,11 @@ void ruch(char t[], char& symbol, char nick1[], char nick2[], bool nastepny)
     char symb1 = 'O';
     char symb2 = 'X';
 
-
     while (runda < 100000) {
+
+        cout << "Nick gracza 1: " << nick1 << endl;
+        cout << "Nick gracza 2: " << nick2 << endl;
+        cout << endl << endl;
         plansza(t);
 
         if (runda % 2 == 1) {
@@ -91,7 +182,7 @@ void ruch(char t[], char& symbol, char nick1[], char nick2[], bool nastepny)
         else {
             nick = nick2;
         }
-        cout << "\nGRACZ " << nick << " : Twoj ruch : ";
+        cout << "\n Tura gracza " << nick << "! Podaj ruch: ";
         cin >> r1;
 
         r = atoi(r1);
@@ -106,25 +197,26 @@ void ruch(char t[], char& symbol, char nick1[], char nick2[], bool nastepny)
             if (symbol == symb1) {
                 nick = nick2;
                 symbol = symb2;
-                cout << "if1" << endl;
             }
             else {
                 symbol = symb1;
                 nick = nick1;
-                cout << "else1" << endl;
             }
 
             runda++;
             nastepny = true;
             t[r] = symbol;
-            cout << "Wolne miejsce" << endl;
         }
         else {
-            cout << "Te pole jest juz zajete, popraw swoj ruch!!!" << endl;
+            cout << "Wybierz poprawne pole!!!" << endl;
             nastepny = false;
         }
 
         cout << "nr rundy: " << runda << endl;
+
+        if (wygrana(t, (runda % 2 == 0 ? symb1 : symb2))) {
+            return;
+        }
     }
 }
 
@@ -141,17 +233,22 @@ int main()
     cin >> nick1;
     cout << "Podaj nick gracza 2: ";
     cin >> nick2;
+
     do
     {
+
         cout << "Gra w Kolko i Krzyzyk dla dwoch graczy\n"
             "======================================\n\n";
         for (int i = 1; i <= 36; i++) p[i] = ' ';
         symbol = symb2;
         nastepny = true;
-        while (!wygrana(p, symb1) && !wygrana(p, symb2) && !remis(p)) ruch(p, symbol, nick1, nick2, nastepny);
+        // while (!wygrana(p, symb1) && !wygrana(p, symb2) && !remis(p)) ruch(p, symbol, nick1, nick2,nastepny);
+        ruch(p, symbol, nick1, nick2, nastepny);
         cout << "Jeszcze raz ? (T = TAK) : ";
         cin >> w;
         cout << "\n\n\n";
 
+
     } while ((w == 'T') || (w == 't'));
+
 }
